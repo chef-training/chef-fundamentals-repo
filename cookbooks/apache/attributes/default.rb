5574e113a11fe121ee6692b1b49cf71b6af3fb13
Michael Goetz <mpgoetz@gmail.com>
2014-03-14 13:16:53 -0500

    Add files from the Template Variables, Notifications and Controlling Idempotency section

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,1 +1,3 @@
 default["apache"]["indexfile"] = "index1.html"
+default["apache"]["sites"]["clowns"] = { "port" => 80 }
+default["apache"]["sites"]["bears"] = { "port" => 81 }
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,8 +15,43 @@ service "httpd" do
   action [ :enable, :start ]
 end
 
-node.default["apache"]["indexfile"] = "index2.html"
-cookbook_file "/var/www/html/index.html" do
-  source node["apache"]["indexfile"]
-  mode "0644"
+# Disable the default virtual host
+execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabled" do 
+  only_if do
+    File.exist?("/etc/httpd/conf.d/welcome.conf")
+  end
+  notifies :restart, "service[httpd]"
+end
+
+# Iterate over the apache sites
+node["apache"]["sites"].each do |site_name, site_data|
+# Set the document root
+  document_root = "/srv/apache/#{site_name}"
+
+# Add a template for Apache virtual host configuration
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
+    source "custom.erb"
+    mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data["port"]
+    )
+    notifies :restart, "service[httpd]"
+  end
+
+# Add a directory resource to create the document_root
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+# Add a template resource for the virtual host's index.html
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    mode "0644"
+    variables(
+      :site_name => site_name,
+      :port => site_data["port"]
+    )
+  end
 end
