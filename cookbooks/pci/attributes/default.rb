66c675e9de57b1825da61c99d7c5e47b8b25d639
Michael Goetz <mpgoetz@gmail.com>
2014-03-14 13:03:30 -0500

    Added files from Attributes, Templates and Cookbook Dependencies section

---
+++ cookbooks/pci/attributes/default.rb
@@ -0,0 +1,1 @@
+default["pci"]["in_scope"] = false
---
+++ cookbooks/pci/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: pci
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
